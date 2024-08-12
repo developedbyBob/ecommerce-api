857e8cf1b4484c30359f94a4a30afec96e6a4d1a
byBob <esdrassantos41@gmail.com>
2024-08-12 20:34:04 -0300

    API Ecomerce

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv"
+import connectDB from "./config/database";
+
+dotenv.config
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+
+connectDB()
+
+app.get('/', (req, res) => {
+    res.send('E-commerce API')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
